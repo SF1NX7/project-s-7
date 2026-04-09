a5918ef7f59aba2eda9ce506021e28740ad6626f
SF1NX <matveeviuriisf@gmail.com>
2026-04-10 01:22:00 +0800

    1

--- script/system/ItemData.gd
+++ script/system/ItemData.gd
@@ -7,9 +7,25 @@ class_name ItemData
 @export var pickup_color: Color = Color(0.615, 0.299, 0.373, 1.0)
 @export_multiline var description: String = ""
 
+# Вкладки инвентаря (фильтр)
 enum ItemClass { WPN, ARM, POT, OTH }
 @export var item_class: ItemClass = ItemClass.OTH
 
-# экипировка
+# Экипировка: в какой слот надевается предмет
 enum EquipSlot { NONE, HEAD, ARMOR, BOOTS, WEAPON, RING }
 @export var equip_slot: EquipSlot = EquipSlot.NONE
+
+# "Профессии/владения": что нужно уметь, чтобы носить/использовать предмет.
+# 0 = любой герой может использовать (ограничений нет).
+@export_flags(
+	"AXE",
+	"SWORD",
+	"MACE",
+	"BOW",
+	"STAFF",
+	"DAGGER",
+	"ROBE",
+	"LIGHT_ARMOR",
+	"HEAVY_ARMOR"
+)
+var required_profs_mask: int = 0
--- script/system/equip_slot_button.gd
+++ script/system/equip_slot_button.gd
@@ -3,6 +3,3 @@ class_name EquipSlotButton
 
 @export var slot_type: ItemData.EquipSlot = ItemData.EquipSlot.NONE
 var equip_slot: String = "NONE"
-
-@export_flags("AXE", "SWORD", "MACE", "BOW", "STAFF", "DAGGER", "ROBE", "LIGHT_ARMOR", "HEAVY_ARMOR")
-var required_profs_mask: int = 0
